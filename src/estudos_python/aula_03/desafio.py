60b3279c353688e118110d9c85c8f276544d439d
Alan Ruiz <alanruiz.eng@gmail.com>
2026-04-04 20:52:24 -0300

    Subindo meus estudos

---
+++ src/estudos_python/aula_03/desafio.py
@@ -0,0 +1,50 @@
+nome_valido = False
+salario_valido = False
+bonus_valido = False
+
+while nome_valido is not True:  
+    try:
+        
+        nome: str = input("Digite o seu nome: ").strip()
+        if not nome:
+            raise ValueError("Valor Vazio. Gentileza Preencher com valores inteiros")
+        elif not nome.replace(" ", "").isalpha():
+            raise ValueError("Valor Inválido no Campo Nome. Gentileza inserir nome em formato de texto")
+        else:
+            nome_valido = True
+
+    except ValueError as e:
+        print(f"[ERRO]: {e}")
+    
+while salario_valido is not True:
+    try:
+        salario = input("Digite o valor do seu salário: ").strip()
+        if not salario:
+            raise ValueError("Valor Vazio. Gentileza Preencher com valores inteiros")
+        elif not salario.isdigit():
+            raise ValueError("Valor Inválido no Campo Salario. Gentileza inserir salario em formato de numero")
+        else:
+            salario_valido = True
+        salario_int = int(salario)
+
+    except ValueError as e:
+        print(f"[ERRO]: {e}")
+
+while bonus_valido is not True:  
+    try:
+        bonus = input("Insira o valor do bonus: ").strip()
+        if not bonus:
+            raise ValueError("Valor Vazio. Gentileza Preencher com valores inteiros")    
+        elif not bonus.isdigit():
+            raise ValueError("Valor Inválido no Campo bonus. Gentileza inserir bonus em formato de numero")
+        else:
+             bonus_valido = True
+        bonus_int = int(bonus)
+
+    except ValueError as e:
+        print(f"[ERRO]: {e}")
+
+bonus_final: int = 1000 + salario_int + bonus_int
+print(bonus_final)
+
+print(f"O {nome} recebeu um salario de {salario} e um bonus de {bonus}% e obteve no final um bonus total de {bonus_final}")
---
+++ src/estudos_python/aula_04/carregar_arquivo.py
@@ -0,0 +1,17 @@
+from ast import mod
+import csv
+
+caminho_do_arquivo: str = "src/estudos_python/aula_04/exemplo.csv"
+
+arquivo_csv: list = []
+
+with open(file = caminho_do_arquivo, mode= "r", encoding = "utf-8") as arquivo:
+    leitor_csv: csv.DictReader = csv.DictReader(arquivo)
+
+    for linha in leitor_csv:
+        arquivo_csv.append(linha)
+
+print(arquivo_csv)
+
+
+
